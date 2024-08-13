d5acae02d19feaa62159c689a288faf60d299f5b
fyodorio <alinetofyodor@gmail.com>
2024-08-13 19:09:51 +0500

    chore: adjust heroku settings

--- prepare-stellar-env.sh
+++ prepare-stellar-env.sh
@@ -6,6 +6,9 @@ chmod +x $0
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
+# Source the environment variables set by rustup
+source $HOME/.cargo/env
+
 # Add the wasm32-unknown-unknown target
 rustup target add wasm32-unknown-unknown
 
