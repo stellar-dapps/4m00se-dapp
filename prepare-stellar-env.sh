61d136fa86b58e8ef6c482701628f6bcb1983edb
fyodorio <alinetofyodor@gmail.com>
2024-08-13 19:03:50 +0500

    chore: heroku provisions

---
+++ prepare-stellar-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set execute permissions for the script
+chmod +x $0
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# Add the wasm32-unknown-unknown target
+rustup target add wasm32-unknown-unknown
+
+# Install the Stellar CLI
+cargo install --locked stellar-cli --features opt
