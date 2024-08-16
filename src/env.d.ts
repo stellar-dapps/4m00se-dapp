b38d0bbe0b7f654fd9f34f79f8d435c2e9240b9f
fyodorio <alinetofyodor@gmail.com>
2024-08-16 23:14:56 +0500

    chore: add env types

---
+++ src/env.d.ts
@@ -0,0 +1,9 @@
+interface ImportMetaEnv {
+  readonly SOROBAN_ACCOUNT: string;
+  readonly SOROBAN_NETWORK: string;
+  readonly VITE_SOROBAN_NETWORK_PASSPHRASE: string;
+  readonly VITE_SOROBAN_RPC_URL: string;
+  readonly VITE_STELLAR_NETWORK_URL: string;
+  readonly VITE_PINATA_JWT: string;
+  readonly VITE_GATEWAY_URL: string;
+}
