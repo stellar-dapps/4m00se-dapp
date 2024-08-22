aaf7db31284f18014a7dd4150fa16216923d4538
fyodorio <alinetofyodor@gmail.com>
2024-08-22 19:51:02 +0500

    feat: update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,13 +35,6 @@
   });
 </script>
 
-<svelte:head>
-  <link
-    rel="icon"
-    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🫎</text></svg>"
-  />
-</svelte:head>
-
 <Header />
 
 {#if isMobile || $page.url.pathname.includes('/docs')}
